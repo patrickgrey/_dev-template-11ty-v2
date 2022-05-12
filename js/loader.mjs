2edc658dda3ffd87f1705cbf66f01b3a56b0a2ec
patrickgrey <websemantic@yahoo.co.uk>
2022-05-12 15:07:13 +0100

    Tried modules but hot reloading not working

---
+++ js/loader.mjs
@@ -0,0 +1,4 @@
+import main from "./main.mjs";
+
+main();
+// console.log(main.init());
---
+++ js/main.mjs
@@ -0,0 +1,8 @@
+export default function main() {
+  // var module = {};
+  // module.init = function () {
+  console.log("now changing this. ");
+  console.log("up and running..........");
+  // };
+  // return module;
+};
